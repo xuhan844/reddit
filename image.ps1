37e45565c5a3b57239416b9a7372ecac6bb0d91b
xuhan844 <fds12345678@gmail.com>
2013-04-21 18:01:28 -0300

    download alien image

---
+++ image.ps1
@@ -0,0 +1,23 @@
+#enter url
+$url = Read-Host "Enter the sub-Reddit URL:"
+
+#get html source code
+$webclient = New-Object System.Net.WebClient
+$storageDir = "C:\downloads"
+$file = "$storageDir\temp.txt"
+$webclient.DownloadFile($url,$file)
+$html = Get-Content "C:\downloads\temp.txt"
+
+#find image url
+$start = "id='header-img' src=`""
+$end = "`" width"
+$html -match "$start(?<content>.*)$end"
+$imgurl = "$matches['content']"
+
+#download image file
+$imgfile = "$storageDir\alien.png"
+$webclient.DownloadFile($imgurl,$imgfile)
+
+Remove-Item c:\downloads\temp.txt
+start C:\downloads
+
